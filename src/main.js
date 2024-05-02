d53864f4cee45d28f26f91298638c6b645f1e1c4
Christof Happ <christof.happ@tngtech.com>
2024-05-02 17:49:47 +0200

    introducing myself

--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Christof Happ',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			"Oh, it's time again for the workshop?"
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: "It's a bit sad",
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'How this idea with the playground repo works'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'reading books', 
+			'cooking', 
+			'playing computer games'
 		]
 	}
 });
